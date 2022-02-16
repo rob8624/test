4329c9a738ee7c1f0bd393a5a30bdb297556c5e6
Rob8624 <robert_melen@yahoo.co.uk>
2022-02-16 09:46:16 +0000

    fixed footer added elvis test post

--- blog/myblog/views.py
+++ blog/myblog/views.py
@@ -35,6 +35,7 @@ def contact_form(request):
             message = form.cleaned_data['message']
             email = form.cleaned_data['email']
             send_mail(name, message, email, ['admin@blog.com'])
+            messages.success(request, 'Your comment will be checked and then added, thankyou.')
             return HttpResponseRedirect(request.path_info)
 
     return render(request, 'post/contact_form.html', {'form': form,
@@ -102,7 +103,7 @@ def post_detail(request, year, month, day, post):
             new_comment.post = post
             #save the comment to the database
             new_comment.save()
-            messages.success(request, 'Your comment will be checked and then added, thankyou.')
+            messages.success(request, 'Your message has been sent, I will reply asap!.')
             return HttpResponseRedirect(request.path_info)
     else:
         comment_form = CommentForm()
